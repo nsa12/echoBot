6e53a2660793943e46c9e3083c5000fa1acea916
nsa12 <singh.navjot12@gmail.com>
2016-09-07 18:46:47 +0530

    chatbot

--- chatbot/views.py
+++ chatbot/views.py
@@ -17,10 +17,11 @@ class MyChatBotView(generic.View):
 		else:
 			return HttpResponse('Oops! Invalid Token')
 
-	@method_decorator(csrf_exempt)
+	@csrf_exempt
 	def dispath(self, request, *args, **kwargs):
 		return generic.View.dispatch(self, request, *args, **kwargs)
 
+	@csrf_exempt
 	def post(self, request, *args, **kwargs):
 		incoming_message = json.loads(self.request.body.decode('utf-8'))
 		print incoming_message
