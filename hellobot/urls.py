49c7a20949454204a9212eaee551b19bb7fbc914
nsa12 <singh.navjot12@gmail.com>
2016-09-07 18:02:59 +0530

    chatbot

--- hellobot/urls.py
+++ hellobot/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index),
-    url(r'^facebook_auth$', MyChatBotView.as_view()),	#because this is a class based view- so have to do as_view
+    url(r'^facebook_auth/?$', MyChatBotView.as_view()),	#because this is a class based view- so have to do as_view
 )
